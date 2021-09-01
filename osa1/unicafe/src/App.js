4e766618bffb085141fe00c2ceb3a4718e6e39cb
Harri Verkkosaari <harri.verkkosaari@tuni.fi>
2021-09-01 09:50:32 +0300

    unicafe step2 valmiina

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,24 +1,54 @@
 import React, { useState } from 'react'
-const Header = (props) => {
-  return [
-    <h1>{props.header}</h1>
-  ]
+
+const sum = (p1,p2) => {
+  return p1+p2
 }
+
+
 const App = () => {
-  const header1 = 'give feedback'
-  const header2 = 'statistics'
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const [all, setAll] = useState(0)
+  const average = 0
+  const [pos, setPos] = useState(0)
+  
+  const header1 = 'give feedback'
+  const header2 = 'statistics'
+  const handleGoodClick = () => {
+    setAll(all + 1)
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () => {
+    setAll(all + 1)
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => {
+    setAll(all + 1)
+    setBad(bad + 1)
+  }
   return (
     <div>
-      <Header header={header1}/>
-
-      <Header header={header2}/>
+      <h1>{header1}</h1>
+      <button onClick={handleGoodClick}>
+        good
+      </button>
+      <button onClick={handleNeutralClick}>
+        neutral
+      </button>
+      <button onClick={handleBadClick}>
+        bad
+      </button>
+      <h1>{header2}</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good-bad)/all}</p>
+      <p>positive {good/all*100} %</p>
     </div>
   )
 }
 
-export default App
+export default App;
